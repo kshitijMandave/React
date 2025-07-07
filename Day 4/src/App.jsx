85849b83f096e9b65518544148a307ca21e029d5
kshitijMandave <kshitij.mandwe@gmail.com>
2025-07-07 23:34:51 +0530

    Today is a day4 with react and, i created my first react app with bundler vite, where i create some components and tried all the things like created element and tried to send props

---
+++ Day 4/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import CompA from "../src/component/CompA/CompA";
+
+function App() {
+  return (
+    <>
+      <CompA name="Kshitij" age="25" />
+    </>
+  );
+}
+
+export default App;
---
+++ Day 4/src/component/CompA/CompA.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CompA(props) {
+  return (
+    <>
+      <h1>Name : {props.name}</h1> <h2>Age : {props.age}</h2>
+    </>
+  );
+}
+
+export default CompA;
